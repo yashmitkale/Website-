1adc4e54f552a07629305f5a50dbfe4b1f8aba4e
yashmitkale <yashmitkale@gmail.com>
2026-08-20 07:53:21 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import "./error-capture";
 
 import { consumeLastCapturedError } from "./lib/error-capture";
-import { renderErrorPage } from "./lib/error-page";
+import { renderErrorPage } from "./error-page";
 
 type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
